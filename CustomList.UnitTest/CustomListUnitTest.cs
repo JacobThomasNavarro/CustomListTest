bd34f72a39d08aa3a4367ddd0b5252f072d198f6
Jake Navarro <jt10navarro@gmail.com>
2019-10-08 10:47:39 -0500

    init push

---
+++ CustomList.UnitTest/CustomListUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomList.UnitTest
+{
+    [TestClass]
+    public class CustomListUnitTest
+    {
+        // unit test for adding multiple items to check position of last item
+        // unit test for adding multiple items to check Count property
+        // unit test for adding number of items beyond 'Capacity' but it still adds
+
+        [TestMethod]
+        public void Add_AddToEmptyList_ItemGoesToIndexZero()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 12;
+            int actual;
+
+            // act
+            testList.Add(12);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddItemToList_CountIncrements()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(234);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CustomList.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomList.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomList.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d3c32d1-edea-46aa-8cc0-2125b6dc7f3c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
